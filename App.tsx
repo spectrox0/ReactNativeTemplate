48e63c703cda7a872505112c3257f7df6a7b7c7b
alejandro velazco <alejanvelazco2008@hotmail.com>
2023-06-16 13:21:56 -0300

    add launch screen sample

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import {CustomThemeProvider} from 'src/context';
-// import {useSplashScreen} from 'src/hooks/useSlashScreen';
 import {MainLayout} from 'src/layouts/Main';
 import React from 'react';
 import {StripeProvider} from '@stripe/stripe-react-native';
 import {config} from 'src/config/config';
 import {NavigationWrapper} from 'src/navigation';
+import {useSplashScreen} from 'src/hooks/useSlashScreen';
 
 function App(): JSX.Element {
+  useSplashScreen();
   return (
     <CustomThemeProvider>
       <MainLayout>
